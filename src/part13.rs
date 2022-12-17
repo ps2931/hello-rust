e45bb0b175233115df4e17704cf9cbb650826aa0
Pankaj Sharma <me.pankaj786@gmail.com>
2022-12-17 19:48:30 +0530

    Closure code

--- src/part13.rs
+++ src/part13.rs
@@ -164,6 +164,125 @@ count_selected_cities(
 count_selected_cities(
     &my_cities,
     |city| city.monster_attack_risk > limit); // also ok
+
+Closure performance:
+Rust's closures are designed to be fast: faster than function pointers. In most
+of the languages. closures are allocated in the heap, dynamically dispatched and
+garbage collected. So creating them, calling them, and collecting them each cost a
+tiny bit of extra CPU time. Moreover, closures tend to rule out inlining, a key
+technique compilers use to eliminate function call overhead and enable a raft of
+other optimizations.
+
+Rust closures have none of these performance drawbacks. Like everything else, they
+are not allocated on the heap unless you put them in the Box, Vec or other container.
+
+
+Closures that drop values:
+let my_str = String::from("Hello");
+let f = || drop(my_str);
+
+When f is called, my_str is dropped.
+
+Calling f() twice will cause double free memory issue. But its not possible in Rust.
+Rust knows this closure cannot be called twice.
+
+FnOnce:
+fn call_twice<F>(closure: F) where F: Fn() {
+    closure();
+    closure();
+}
+
+Fn() is a shorthand for Fn() -> (). That is, closures that no arguments and return ().
+
+let my_str = String::from("hello");
+let f = || drop(my_str);
+call_twice(f);
+
+This will cause double free issue. The error will be:
+
+Expected a closure that implements the `Fn` trait, but this closure only implements `FnOnce`.
+
+Closures that drop values, like f are not allowed to have Fn. They are, quite
+literally, no Fn at all. They implement a less powerfull trait, FnOnce, the trait
+closures that can be called once.
+
+Just as an arithmetic expression like `a+b` is shorthand for a method call,
+Add::add(a, b), Rust treats closure() as shorthand for one of the two trait methods:
+Fn() or FnOnce().
+
+For a Fn closure, closure() exapands to closure.call().
+For a closure safe to be called once, closure() will expand to closure.call_once().
+
+let dict = produce_glossary();
+let debug_dum_dict = || {
+    for (key, value) in dict {
+        println!("{:?} - {:?}", key, value);
+    }
+};
+
+Calling debug_dum_dict twice will cause the same issue.
+
+To debug this, we have to figure out why the closure is a FnOnce. Which value is
+being used up here. The only thing we're referring to as all is dict. We're using
+up dict by iterating over it directly. We should be looping over &dict rather
+than plain dict,to access the values by reference.
+
+let debug_dump_dict = ||{
+    for (key, value) in &dict {
+        println!("{:?} - {:?}", key, value);
+    }
+};
+
+
+FnMut:
+There is one more kind of closure, the kind that contains mutable data or mut references.
+
+Rust considers non-mut values thread safe. But it wouldn't be safe to share non-mut
+closures that contain mut data. Therefore Rust has FnMut, a category of closures
+that write.
+
+trait FnMut() -R {
+    fn call_mut(&mut self) -> R;
+}
+
+Any closure that requires mut access to a value, but doesn't drop any values,
+is a FnMut closure.
+
+let mut i = 0;
+let incr = || {
+    i += 1; // incr borrows a mut references to i
+    println!("Ding! i is now {}", i);
+};
+call_twice(incr);
+
+The way we wrote call_twice, it requires a Fn. Since incr is a FnMut and not a Fn,
+this code fails to compile.
+
+To recap:
+Fn: closures and functions that you can call multiple times.
+FnMut: closures that can be called multiple times if the closure itself is declared mut.
+FnOnce: closures that can be called once, if the caller owns the closure.
+
+Every Fn meets the requirements for FnMut, and every FnMut meets the requirements for FnOnce.
+Instead Fn() is a sub trait of FnMut(), which is a sub trait of FnOnce(). This makes
+Fn() the most exclusive and most powerful category.
+
+We can change call_twice function signature to:
+fn call_twice<F>(mut closure: F) where F: FnMut() {
+    closure();
+    closure();
+}
+
+With this change, we still accept all Fn closures and we additionally can use call_twice
+on closures that mutate data.
+
+let mut i = 0;
+call_twice(|| i += 1); // Ok
+assert_eq!(i, 2);
+
+
+Callbacks:
+
 */
 
 
